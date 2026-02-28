10e96240985d57c79c8d0607d2fc7b18573555d8
Bruins288 <bruins288@gmail.com>
2026-02-28 17:57:18 +0300

    День 2: изучил переменные и типы данных в Python

---
+++ day2/tasks/customs_calculator.py
@@ -0,0 +1,20 @@
+#Задание 1 (Проект 2 - таможня):
+#Напиши программу, которая:
+#1. Спрашивает у пользователя название товара.
+#2. Спрашивает стоимость товара в долларах.
+#3. Спрашивает курс доллара к рублю.
+#4. Переводит стоимость в рубли.дрель
+#5. Выводит итоговое сообщение: "Товар [название] стоит [сумма] рублей."
+
+print("ПРОГРАММА КАЛЬКУЛЯТОР ТОВАРОВ")
+print("\n"+"="*50)
+
+product_name = input("Введите название товара: ")
+product_cost_usd = float(input("Введите стоимость товара в USD: "))
+exchange_rate = float(input("Введите курс USD/RUB: "))
+product_cost_rub = product_cost_usd * exchange_rate
+product_duty = product_cost_rub * (15/100)
+product_vat = (product_cost_rub + product_duty)*(22/100)
+print(f"Товар {product_name} стоит {product_cost_rub} рублей.")
+print(f"Пошлина 15%: {product_duty} рублей.")
+print(f"НДС 22%: {product_vat} рублей.")
